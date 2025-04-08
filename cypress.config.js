01457e1c46238832a98f7ad257671588e51e9c19
Allan Paul A. Umotoy <allanumotoy1600@gmail.com>
2025-04-08 10:11:51 +0800

    add/update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 // npx cypress run --record --key c9d6fd8a-320f-4bee-a6ac-81ebc5c6630c
+// npx cypress run --record --key c9d6fd8a-320f-4bee-a6ac-81ebc5c6630c
 
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "dg7adv",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
